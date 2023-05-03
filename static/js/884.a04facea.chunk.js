c06f04fbe2bdc04462a993565977b2555d709f4a
iSteffan <112157511+iSteffan@users.noreply.github.com>
2023-05-03 08:34:19 +0000

    Deploying to gh-pages from @ iSteffan/goit-react-hw-05-movies@74b88c87ca29bc7260b2e63fb44c2dac38985483 🚀

---
+++ static/js/884.a04facea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[884],{884:function(e,r,t){t.r(r);var n=t(791),a=t(689),c=t(184);r.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(n.Suspense,{fallback:(0,c.jsx)("div",{children:"LOADING SUBPAGE..."}),children:(0,c.jsx)(a.j3,{})})})}}}]);
+//# sourceMappingURL=884.a04facea.chunk.js.map
